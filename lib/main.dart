d28f6a9f8be13e506774ddef336d50731f5d941d
jessmathews190 <jessmathews190@gmail.com>
2024-12-14 12:01:03 +0000

    add const and super parameters

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:myapp/screens/home.dart';
 
@@ -6,7 +8,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  const Home({super.key});
 
 // Suggested code may be subject to a license. Learn more: ~LicenseLog:1564332017.
   @override
@@ -10,7 +12,7 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
+          children: const [
             Icon(
               Icons.menu,
               color: Color.fromRGBO(0, 0, 0, 1.0),
